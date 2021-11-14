8495726fb0d5deb741d177c38925504886f9002a
Tadej <tadej.996@gmail.com>
2021-11-14 11:19:16 +0100

    Add ability to join rooms and recieve messages.

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const { Server } = require("socket.io");
 
 const namespaces = require('./namespaces.json');
 
+const findActiveRoom = (namespaceSocket, namespace) => {
+  const roomSlug = Array.from(namespaceSocket.rooms)[1]
+  return namespace.rooms.find(room => room.slug === roomSlug)
+};
+
 const expressServer = app.listen(3001);
 const io = new Server(expressServer, {
   cors: {
@@ -15,7 +20,7 @@ const io = new Server(expressServer, {
 io.on('connection', (socket) => {
   console.log(`User ${socket.id} connected to ${socket.nsp.name}`);
 
-  socket.emit('namespaces', namespaces)
+  socket.emit('load:namespaces', namespaces)
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
@@ -24,6 +29,24 @@ io.on('connection', (socket) => {
 namespaces.forEach(namespace => {
   io.of(namespace.endpoint).on('connection', namespaceSocket => {
     console.log(`User ${namespaceSocket.id} connected to ${namespaceSocket.nsp.name}`);
-    namespaceSocket.emit('rooms', namespace.rooms)
+    namespaceSocket.on('message', (message) => {
+      console.log(`User ${namespaceSocket.id} in ${namespaceSocket.nsp.name} sent a message: ${message}`);
+      const activeRoom = findActiveRoom(namespaceSocket, namespace)
+      activeRoom.messages.push({
+        user: 'Jaka',
+        content: message,
+      })
+      io.of(namespace.endpoint).to(activeRoom.slug).emit('load:messages', activeRoom.messages)
+    })
+    namespaceSocket.on('room:join', (room) => {
+      console.log(`User ${namespaceSocket.id} in ${namespaceSocket.nsp.name} joined a room: ${room.slug}`);
+      const activeRoom = findActiveRoom(namespaceSocket, namespace)
+      if (activeRoom) {
+        namespaceSocket.leave(activeRoom.slug)
+      }
+
+      namespaceSocket.join(room.slug)
+    })
+    namespaceSocket.emit('load:rooms', namespace.rooms)
   })
 })
