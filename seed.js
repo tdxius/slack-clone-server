72ac818be3056e5fd57f88178630cc29912339f7
Tadej <tadej.996@gmail.com>
2021-11-15 14:05:44 +0100

    Seed images for namespaces and rooms.

--- seed.js
+++ seed.js
@@ -1,32 +1,34 @@
 const faker = require('faker/locale/en');
 const mongo = require('./mongo.js');
 
+const generateNamespace = () => {
+  const namespaceTitle = faker.company.companyName()
+  return {
+    icon: faker.image.image(),
+    title: namespaceTitle,
+    endpoint: `/${faker.helpers.slugify(namespaceTitle).toLowerCase()}`,
+    rooms: [...Array(Math.floor(Math.random() * 5) + 1)].map(() => {
+      const roomTitle = faker.company.companyName()
+      return {
+        icon: faker.image.image(),
+        title: roomTitle,
+        slug: faker.helpers.slugify(roomTitle).toLowerCase(),
+        messages: [...Array(Math.floor(Math.random() * 5) + 1)].map(() => ({
+          user: faker.internet.userName(),
+          content: faker.lorem.sentence(),
+        }))
+      };
+    })
+  }
+}
+
 (async () => {
   const db = await mongo()
   const collection = db.collection('namespaces');
 
   console.warn('Seeding namespaces...')
 
-  const documents = [...Array(3)].map(() => {
-    const namespaceTitle = faker.company.companyName()
-    return {
-      icon: "mdi-briefcase",
-      title: namespaceTitle,
-      endpoint: `/${faker.helpers.slugify(namespaceTitle).toLowerCase()}`,
-      rooms: [...Array(Math.floor(Math.random() * 5) + 1)].map(() => {
-        const roomTitle = faker.company.companyName()
-        return {
-          icon: "mdi-briefcase",
-          title: roomTitle,
-          slug: faker.helpers.slugify(roomTitle).toLowerCase(),
-          messages: [...Array(Math.floor(Math.random() * 5) + 1)].map(() => ({
-            user: faker.internet.userName(),
-            content: faker.lorem.sentence(),
-          }))
-        };
-      })
-    }
-  })
+  const documents = [...Array(3)].map(() => generateNamespace())
 
   await collection.deleteMany({})
   await collection.insertMany(documents)
